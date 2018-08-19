ea36b016bb6e514851143afcf0c4d1ad9d2a76f0
Paul Gheorghe <paul_gheorghe2000@yahoo.com>
2018-08-19 14:59:48 +0300

    develop: volumetric light shader; investigate why min has to be scaled down

---
+++ res/shaders/volumetricLight.fs
@@ -0,0 +1,208 @@
+#version 330
+
+layout(location = 0) out vec4 outLight;
+
+uniform sampler2D depthSampler;
+uniform sampler2D volumetricLightDepthSampler;
+uniform sampler2D blueNoiseSampler;
+
+uniform mat4 volumetricLightProjectionMatrix;
+uniform mat4 volumetricLightViewMatrix;
+
+uniform vec3 lightColor;
+uniform vec3 lightPosition;
+//uniform vec3 lightDir;
+//uniform vec3 lightRadius;
+uniform int screenWidth, screenHeight;
+
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+in vec3 viewRay;
+flat in vec3 lightPositionEyeSpace;
+
+const int numSamplePoints = 60;
+const float lightRadius = 600;
+const float alpha = 0.3;
+
+bool coneIntersection(vec3 rayDir, inout float t1, inout float t2)
+{
+    vec3 lightDir = normalize(mat3(viewMatrix) * mat3(modelMatrix) * vec3(0,0,-1));
+    vec3 negateLightPositionEyeSpace = -(lightPositionEyeSpace);
+    float cos_alpha2 = pow(cos(alpha), 2); // 0.91266780745 = cos(0.3) ^ 2
+    float light_ray_dot = dot(lightDir, rayDir);
+    float light_pos_dir_dot = dot(negateLightPositionEyeSpace, lightDir);
+    float ray_dir_light_pos_dot = dot(negateLightPositionEyeSpace, rayDir);
+
+    float a = light_ray_dot * light_ray_dot - cos_alpha2;
+    float b = 2.0 * (light_pos_dir_dot * light_ray_dot - ray_dir_light_pos_dot * cos_alpha2);
+    float c = light_pos_dir_dot * light_pos_dir_dot - dot(negateLightPositionEyeSpace, negateLightPositionEyeSpace) * cos_alpha2;
+
+    float delta = b*b - 4.0*a*c;
+
+    if (delta < 0) {
+        return false;
+    } else if (delta == 0) {
+        t1 = -b / (2 * a);
+        t2 = t1;
+    } else {
+        t1 = (-b + sqrt(delta)) / (2*a);
+        t2 = (-b - sqrt(delta)) / (2*a);
+    }
+
+    return true;
+}
+
+bool sphereIntersection(vec3 rayDir, inout float t1, inout float t2)
+{
+    vec3 negateLightPositionEyeSpace = -(lightPositionEyeSpace);
+    float a = 1;
+    float b = 2.0 * dot(rayDir, negateLightPositionEyeSpace);
+    float c = dot(negateLightPositionEyeSpace,negateLightPositionEyeSpace) - lightRadius * lightRadius;
+
+    float delta = b*b - 4.0*a*c;
+
+    if (delta < 0) {
+        return false;
+    } else if (delta == 0) {
+        t1 = -b / (2 * a);
+        t2 = t1;
+    } else {
+        t1 = (-b + sqrt(delta)) / (2*a);
+        t2 = (-b - sqrt(delta)) / (2*a);
+    }
+
+    return true;
+}
+
+bool spotLightIntersection(vec3 rayDir, inout float tmin, inout float tmax)
+{
+    float tc1, tc2, ts1, ts2;
+    /* why does this need to be normalized?? */
+    /* it's in view space, the same for eyeZNorm */
+    rayDir = normalize(rayDir);
+    bool coneIntersect = coneIntersection(rayDir, tc1, tc2);
+    if (!coneIntersect)
+        return false;
+    bool sphereIntersect = false;
+
+    vec3 lightDir = normalize(mat3(viewMatrix) * mat3(modelMatrix) * vec3(0,0,-1));
+
+    sphereIntersect = sphereIntersection(rayDir, ts1, ts2);
+    if (!sphereIntersect)
+        return false;
+
+	vec3 c1 = rayDir * tc1;
+	vec3 c2 = rayDir * tc2;
+	vec3 s1 = rayDir * ts1;
+	vec3 s2 = rayDir * ts2;
+
+	vec3 coneToC1 = c1 - lightPositionEyeSpace;
+	vec3 coneToC2 = c2 - lightPositionEyeSpace;
+	vec3 coneToS1 = s1 - lightPositionEyeSpace;
+	vec3 coneToS2 = s2 - lightPositionEyeSpace;
+
+	float hits[4]; // Needs 4 for literal edge cases.
+	int index = 0;
+	float coneCosAngle = cos(alpha);
+
+	hits[0] = 0;
+	hits[1] = 0;
+	hits[2] = 0;
+	hits[3] = 0;
+
+	if (dot(coneToC1, lightDir) > 0 && length(coneToC1) < lightRadius) {
+		hits[index++] = tc1;
+	}
+	if (dot(coneToC2, lightDir) > 0 && length(coneToC2) < lightRadius) {
+		hits[index++] = tc2;
+	}
+	if (dot(normalize(coneToS1), lightDir) > coneCosAngle) {
+		hits[index++] = ts1;
+	}
+	if (dot(normalize(coneToS2), lightDir) > coneCosAngle) {
+		hits[index++] = ts2;
+	}
+
+	float closest = min(hits[0], hits[1]);
+	float farthest = max(hits[0], hits[1]);
+
+	tmin = max(0, closest);
+	tmax = max(0, farthest);
+
+	return index == 2 && farthest >= 0;
+}
+
+void main() {
+    vec2 texCoord = gl_FragCoord.xy / (vec2(screenWidth, screenHeight)/2);
+    vec3 view = vec3(viewRay.xy/viewRay.z, 1.0);
+    float depth = texture(depthSampler, texCoord).x;
+
+    float eyeZNorm = -projectionMatrix[3][2]/((2.0 * depth - 1.0) + projectionMatrix[2][2]);
+
+    float tmin, tmax;
+    float pointsNotInShadow = 0;
+
+    mat4 inverseViewMatrix = inverse(viewMatrix);
+
+    bool spotIntersect = spotLightIntersection(-view, tmin, tmax);
+
+    if (!spotIntersect) {
+        discard;
+    }
+
+    tmin = -tmin;//* 0.53;
+    tmax = -tmax;
+
+    if (tmax < eyeZNorm) {
+        tmax = eyeZNorm;
+    }
+
+    if (tmin < eyeZNorm) {
+        discard;
+    }
+
+    float rayLength = abs(tmax - tmin);
+    float sampleLength = rayLength / numSamplePoints;
+
+    float begin = (rayLength == 0 ? numSamplePoints : 0);
+
+    for (float t = begin; t <= numSamplePoints; t++) {
+        float currentT = tmin - sampleLength * t;
+        vec3 viewPositionLocal = view * currentT;
+
+        vec4 worldPosition = (inverseViewMatrix * vec4(viewPositionLocal, 1.0));
+        vec4 volumetricLightClip = (volumetricLightProjectionMatrix * volumetricLightViewMatrix * worldPosition);
+        vec3 volumetricLightNDC = volumetricLightClip.xyz / volumetricLightClip.w;
+        vec3 volumetricLightNDCNormalized = volumetricLightNDC.xyz * 0.5f + 0.5f;
+
+        float eyeZObjectDepth = volumetricLightNDCNormalized.z;
+        float linearObjectDepth = -volumetricLightProjectionMatrix[3][2]/(eyeZObjectDepth + volumetricLightProjectionMatrix[2][2]);
+
+        float currentDepth = texture (volumetricLightDepthSampler, volumetricLightNDCNormalized.xy).x;
+        float linearEyeZODepthMap = -volumetricLightProjectionMatrix[3][2]/(currentDepth + volumetricLightProjectionMatrix[2][2]);
+
+        float volumetricLightAttCoef = dot(normalize(worldPosition.xyz - lightPosition),
+                                            normalize(mat3(modelMatrix) * vec3(0,0,-1)));
+
+        vec3 dir = worldPosition.xyz - lightPosition;
+        float len = length(dir);
+
+        float dist1 = lightRadius*0.5;
+        float dist2 = lightRadius*0.3;
+        float alpha1 = cos(alpha);
+        float alpha2 = cos(0.1);
+
+        float sm1 = smoothstep(1.0, 0, (len-dist2)/(dist1-dist2));
+        float sm2 = smoothstep(1.0, 0, (volumetricLightAttCoef-alpha2)/(alpha1-alpha2));
+
+        if (linearEyeZODepthMap < linearObjectDepth-0.9) {
+            pointsNotInShadow += sm1 * sm2;
+        }
+    }
+
+    outLight.xyz = lightColor;
+    outLight.a = min(pointsNotInShadow / numSamplePoints, 0.8);
+    outLight.xyz *= outLight.a;
+}
